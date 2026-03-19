33d48541c73ddfa071f090a10e3fefe032286134
leirdan <uouleirdan@gmail.com>
2026-03-19 15:44:25 -0300

    first commit
    - cmake file basic configuration for now;
    - build directory;
    - added entrypoint (main.cpp);
    - some code just to assert that cmake is working

---
+++ src/lexer.cpp
@@ -0,0 +1,6 @@
+#include "lexer.hpp"
+
+void Lexer::print()
+{
+  std::cout << "Hello from lexer\n";
+}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "lexer.hpp"
+
+int main() 
+{
+  std::cout << "Hello, world!\n";
+  Lexer l;
+  l.print();
+  return 0;
+}
