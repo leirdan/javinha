bb17defb4698d37dfd088ab692469091d4f4d24a
Paz <maria.paz.marcato.719@ufrn.edu.br>
2026-04-25 16:35:12 -0300

    feat(token): enhance TokenType enum and add to_string function for better token representation

--- include/token.hpp
+++ include/token.hpp
@@ -3,16 +3,28 @@
 
 namespace jc {
     enum class TokenType {
-        IDENTIFIER,
-        KEYWORD,
-        NUMBER,
-        STRING,
-        OPERATOR,
-        DELIMITER,
-        END_OF_FILE,
-        UNKNOWN
+        IDENTIFIER,   
+        KEYWORD,      
+        NUMBER,       
+        STRING,       
+        OPERATOR,     
+        DELIMITER,    
+        END_OF_FILE,  
+        UNKNOWN      
     };
 
+    inline std::string to_string(TokenType t) {
+        switch (t) {
+            case TokenType::IDENTIFIER: return "IDENTIFIER";
+            case TokenType::KEYWORD: return "KEYWORD";
+            case TokenType::NUMBER: return "NUMBER";
+            case TokenType::STRING: return "STRING";
+            case TokenType::OPERATOR: return "OPERATOR";
+            case TokenType::DELIMITER: return "DELIMITER";
+            case TokenType::END_OF_FILE: return "EOF";
+            default: return "UNKNOWN";
+        }
+    }
     struct Token {
         TokenType type;
         std::string value;
